987ad344bd81ade66f0bc79c426701e283808712
shobhitag001 <shobhitag001@gmail.com>
2026-02-26 15:25:08 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,5 +7,23 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully.");
+
+            // Hardcoded string
+            String input = "madam";
+
+            boolean isPalindrome = true;
+
+            // Loop only till half of the string length
+            for (int i = 0; i < input.length() / 2; i++) {
+
+                if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            // Display result
+            System.out.println("Input text: " + input);
+            System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
