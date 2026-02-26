17297eb5dd549e23c3c11b95d2f93c5fe496adbf
shobhitag001 <shobhitag001@gmail.com>
2026-02-26 16:17:09 +0530

    UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,33 +1,41 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         // Input string
-        String input = "madam";
+        String input = "level";
 
-        // Create a stack of characters
+        // Create Queue (FIFO)
+        Queue<Character> queue = new LinkedList<>();
+
+        // Create Stack (LIFO)
         Stack<Character> stack = new Stack<>();
 
-        // Push characters into stack
+        // Enqueue and Push characters
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            queue.add(ch);      // Enqueue operation
+            stack.push(ch);     // Push operation
         }
 
         // Assume palindrome initially
         boolean isPalindrome = true;
 
-        // Pop characters and compare
-        for (int i = 0; i < input.length(); i++) {
+        // Compare dequeue (queue) and pop (stack)
+        while (!queue.isEmpty()) {
 
-            char poppedChar = stack.pop();
+            char fromQueue = queue.remove(); // Dequeue operation
+            char fromStack = stack.pop();    // Pop operation
 
-            if (input.charAt(i) != poppedChar) {
+            if (fromQueue != fromStack) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        // Print output
+        // Print result
         if (isPalindrome) {
             System.out.println("The word \"" + input + "\" is a Palindrome.");
         } else {
